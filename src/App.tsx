f8ea5f331c7a0dc04a38e5142fa635e7f68ba80c
Pawel <pawel-czarnecki@o2.pl>
2023-03-08 08:39:51 +0000

    added main components and store for state managment

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import AttentionSection from './components/AttentionSection'
+import LangChanger from './components/LangChanger'
+import { LangContextProvider } from './store/lang-context'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return <div className="App"></div>
+  return (
+    <LangContextProvider>
+      <div className="App">
+        <AttentionSection sectionName="attention" />
+        <LangChanger />
+      </div>
+    </LangContextProvider>
+  )
 }
 
 export default App
